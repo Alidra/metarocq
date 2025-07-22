f749200285a526852b3c1a0f6b8da0e5d7c8d024
Abdelghani ALIDRA <alidrandco@yahoo.fr>
2025-07-22 13:25:30 +0200

    fix branch in trigger

--- .nix/config.nix
+++ .nix/config.nix
@@ -5,7 +5,7 @@
   bundles."rocq-9.0" = {
     rocqPackages.rocq-core.override.version = "9.0";
     rocqPackages.equations.override.version = "1.3.1-9.0";
-    push-branches = ["9.0"];
+    push-branches = ["withnix2"];
   };
   cachix.coq = {};
   cachix.coq-community = {};
